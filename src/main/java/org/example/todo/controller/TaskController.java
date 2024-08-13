a0799aadbd8631c7e995c84ec35cdba7f319bc6b
Администратор <rapilbek.alan@bk.ru>
2024-08-13 14:53:37 +0500

    Rew

--- src/main/java/org/example/todo/controller/TaskController.java
+++ src/main/java/org/example/todo/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.example.todo.repository.TaskRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +22,31 @@ public class TaskController {
         }
         return taskRepository.findAll();
     }
-
+    @GetMapping("/completed")
+    public List<Task> getCompletedTask(){
+        if(taskRepository.findAll().stream().noneMatch(Task::isCompleted)){
+            List<Task> x = new ArrayList<>();
+            return x;
+        }
+        return taskRepository.findAll().stream().filter(Task::isCompleted).toList();
+    }
+    @GetMapping("/toBeDone")
+    public List<Task> getToBeDoneTask(){
+        if(taskRepository.findAll().stream().noneMatch(c-> !c.isCompleted())){
+            List<Task> x = new ArrayList<>();
+            return x;
+        }
+        return taskRepository.findAll().stream().filter(c-> !c.isCompleted()).toList();
+    }
+    @GetMapping("/getById")
+    public Object getById(@PathVariable int id){
+        if(taskRepository.findById((long) id).isEmpty()){
+            HashMap<String,String> map = new HashMap<>();
+            map.put("message","This task do not exists");
+            return map;
+        }
+        return taskRepository.findById((long) id);
+    }
     @PostMapping
     public Task createTask(@RequestBody Task task) {
         return task;
