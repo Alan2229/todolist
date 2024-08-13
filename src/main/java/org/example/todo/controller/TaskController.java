108a34b963ca05a01ea5d212d604c194301cd92c
madinaAbd <abdulgafarova.madina@mail.ru>
2024-08-13 14:28:16 +0500

    2nd commit

--- src/main/java/org/example/todo/controller/TaskController.java
+++ src/main/java/org/example/todo/controller/TaskController.java
@@ -1,4 +1,16 @@
 package org.example.todo.controller;
 
+import lombok.Getter;
+import org.example.todo.model.Task;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class TaskController {
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return task;
+    }
 }
