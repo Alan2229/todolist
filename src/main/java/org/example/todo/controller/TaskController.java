0ee0d6789ff1936ced026bf4ce781420d7e3d97e
Администратор <rapilbek.alan@bk.ru>
2024-08-13 14:26:22 +0500

    Repository ans Task Controller

--- src/main/java/org/example/todo/controller/TaskController.java
+++ src/main/java/org/example/todo/controller/TaskController.java
@@ -1,13 +1,27 @@
 package org.example.todo.controller;
 
 import lombok.AllArgsConstructor;
+import org.example.todo.model.Task;
 import org.example.todo.repository.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("task")
 public class TaskController {
     private TaskRepository taskRepository;
+    @GetMapping("/all")
+    public List<Task> getAllTask(){
+        if(taskRepository.findAll().isEmpty()){
+            List<Task> ans = new ArrayList<>();
+            return ans;
+        }
+        return taskRepository.findAll();
+    }
 }
--- src/main/java/org/example/todo/repository/TaskRepository.java
+++ src/main/java/org/example/todo/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package org.example.todo.repository;
 
 import org.example.todo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
-
 }
